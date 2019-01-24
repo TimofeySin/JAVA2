98aa274b938f91d76bf5e7e0f9fbf34d0c42542c
TimofeySin <TimofeySin@users.noreply.github.com>
2019-01-24 23:47:37 +0300

    Delete Obstacle.java

--- src/ru/geekbrains/Jawa2/Lesson1/surface/Obstacle.java
+++
@@ -1,9 +0,0 @@
-package ru.geekbrains.Jawa2.Lesson1.surface;
-
-import ru.geekbrains.Jawa2.Lesson1.participant.Competitor;
-
-
-public abstract class Obstacle {
-
-    public abstract void doIt(Competitor competitor);
-}
