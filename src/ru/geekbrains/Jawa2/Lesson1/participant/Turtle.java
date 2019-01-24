1243d6574b1aeb3169e1138bb6dfd142679d35ff
TimofeySin <TimofeySin@users.noreply.github.com>
2019-01-24 23:47:04 +0300

    Delete Turtle.java

--- src/ru/geekbrains/Jawa2/Lesson1/participant/Turtle.java
+++
@@ -1,7 +0,0 @@
-package ru.geekbrains.Jawa2.Lesson1.participant;
-
-public class Turtle extends Animal {
-    public Turtle(String name, String color, int maxRunDistance, int maxSwimDistance, int maxJumpHeight) {
-        super(name, color, maxRunDistance, maxSwimDistance, maxJumpHeight,"Черепашка");
-    }
-}
