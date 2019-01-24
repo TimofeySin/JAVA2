ff8a9b910644f0045ad130f9cd2c4b828b1e95c5
TimofeySin <TimofeySin@users.noreply.github.com>
2019-01-24 23:46:14 +0300

    Delete Fish.java

--- src/ru/geekbrains/Jawa2/Lesson1/participant/Fish.java
+++
@@ -1,8 +0,0 @@
-package ru.geekbrains.Jawa2.Lesson1.participant;
-
-public class Fish extends Animal {
-
-    public Fish(String name, String color, int maxRunDistance, int maxSwimDistance, int maxJumpHeight) {
-        super(name, color, maxRunDistance, maxSwimDistance, maxJumpHeight,"Рыбка");
-    }
-}
