77203429a258a83c4ffc799345f22bc1b5f2fb0e
TT <timofeysin@gmail.com>
2019-01-28 14:49:59 +0300

    Lesson4

--- src/ru/geekbrains/Jawa2/Lesson4/Lesson4.java
+++ src/ru/geekbrains/Jawa2/Lesson4/Lesson4.java
@@ -1,14 +1,23 @@
 package ru.geekbrains.Jawa2.Lesson4;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
+
 
 import java.io.IOException;
 
 public class Lesson4 extends Application {
+
+    public TextField inputTextStr;
+    public TextArea bigField;
+
 
     public static void main(String[] args) {
         launch(args);
@@ -18,10 +27,20 @@ public class Lesson4 extends Application {
     public void start(Stage primaryStage) throws IOException {
         Parent parent = FXMLLoader.load(getClass().getResource("Les4.fxml"));
 
+        Image image = new Image("file:src\\ru\\geekbrains\\Jawa2\\Lesson4\\icon.png");
+
+        primaryStage.getIcons().add(image);
         primaryStage.setTitle("Alone in the chat.");
         primaryStage.setScene(new Scene(parent));
         primaryStage.show();
     }
 
+    public void pressKey(ActionEvent actionEvent) {
+        StringBuffer stringBuffer = new StringBuffer();
+        stringBuffer.append(bigField.getText());
+        bigField.setText(stringBuffer.toString() + inputTextStr.getText()+"\n");
+        inputTextStr.clear();
 
+    }
 }
+
