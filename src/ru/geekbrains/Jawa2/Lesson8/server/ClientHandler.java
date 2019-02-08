94660a1f001600842e686e39b07692892912d65a
TT <timofeysin@gmail.com>
2019-02-09 00:07:37 +0300

    Lesson8 in

---
+++ src/ru/geekbrains/Jawa2/Lesson8/server/ClientHandler.java
@@ -0,0 +1,70 @@
+package lesson7.server;
+
+import lesson7.server.model.Client;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+public class ClientHandler {
+
+    private Server server;
+    private Socket socket;
+    private DataInputStream in;
+    private DataOutputStream out;
+    private Client client;
+
+    public ClientHandler(Server server, Socket socket, Client client) {
+        this.server = server;
+        this.socket = socket;
+        this.client = client;
+
+        this.in = null;
+        this.out = null;
+        try {
+            in = new DataInputStream(socket.getInputStream());
+            out = new DataOutputStream(socket.getOutputStream());
+
+            new Thread(() -> {
+                try {
+                    while (true) {
+                        String line = in.readUTF();
+
+                        if (line.startsWith("/")){
+                            if (line.startsWith("/w ")){
+                                String[] tokens = line.split(" ", 3);
+                                server.sendTo(this, tokens[1], tokens[2]);
+                            }
+                            if (line.startsWith("/block ")){
+                                String[] tokens = line.split(" ", 2);
+                                client.getBlockList().add(tokens[1].trim());
+                            }
+                        }else {
+                            System.out.print("from " + client.getNick() + line);
+                            server.broadCast(this,client.getNick() + ": " + line);
+                        }
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    server.unsubscribe(this);
+                }
+            }).start();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void sendMsg(String msg) {
+        try {
+            out.writeUTF(msg);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Client getClient() {
+        return client;
+    }
+}
---
+++ src/ru/geekbrains/Jawa2/Lesson8/server/Server.java
@@ -0,0 +1,89 @@
+package lesson7.server;
+
+import lesson7.server.impl.CsvAuthService;
+import lesson7.server.model.Client;
+import lesson7.server.task.AuthenticationTask;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Server {
+
+    private List<ClientHandler> clients = Collections.synchronizedList(new ArrayList<>());
+
+    public Server() {
+        ServerSocket server = null;
+        Socket socket = null;
+        try {
+
+            server = new ServerSocket(8080);
+            System.out.println("server start");
+
+            while (true) {
+                socket = server.accept();
+                System.out.println("client connected");
+                new AuthenticationTask(socket, this, new CsvAuthService()).start();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (socket != null) {
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (server != null) {
+                try {
+                    server.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public void broadCast(ClientHandler from, String msg) {
+        for (ClientHandler handler : clients) {
+            if (!handler.getClient().getBlockList().contains(from.getClient().getNick())) {
+                handler.sendMsg(msg);
+            }
+        }
+    }
+
+    public void userList() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("/users ");
+        for (ClientHandler clientHandler: clients){
+            sb.append(clientHandler.getClient().getNick()).append(" ");
+        }
+        for (ClientHandler clientHandler: clients){
+            clientHandler.sendMsg(sb.toString());
+        }
+    }
+
+    public void subscribe(ClientHandler client) {
+        clients.add(client);
+        userList();
+    }
+
+    public void unsubscribe(ClientHandler client) {
+        clients.remove(client);
+        userList();
+    }
+
+    public void sendTo(ClientHandler fromHandler, String toNick, String message) {
+        clients.forEach(clientHandler -> {
+            if (clientHandler.getClient().getNick().equals(toNick)) {
+                fromHandler.sendMsg("to " + toNick + ": " + message);
+                clientHandler.sendMsg("from " + fromHandler.getClient().getNick() + ": " + message);
+            }
+        });
+    }
+}
